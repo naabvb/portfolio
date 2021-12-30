14cb8394b067a8d12995f6dfdd8d91a7bcbbcf35
naabvb <46524999+naabvb@users.noreply.github.com>
2021-12-30 23:21:14 +0200

    Add nextjs + ChakraUI starter

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "../styles/globals.css";
+
+const config = {
+  initialColorMode: "dark",
+  useSystemColorMode: false,
+};
+
+const theme = extendTheme({ config });
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
