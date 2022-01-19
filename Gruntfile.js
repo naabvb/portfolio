7b7c45e9791fe1cdfa955b98782871a8d3e6437d
naabvb <46524999+naabvb@users.noreply.github.com>
2022-01-19 17:55:07 +0200

    Add robots.txt and optimize Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
     compress: {
-      js_gz: {
+      create_gz: {
         options: {
           mode: "gzip",
           level: 9,
@@ -11,46 +11,12 @@ module.exports = function (grunt) {
         cwd: "out/",
         dest: "out/",
         extDot: "last",
-        src: ["**/*.js"],
-        ext: ".js.gz",
-      },
-      css_gz: {
-        options: {
-          mode: "gzip",
-          level: 9,
+        src: ["**/*.{js,css,html,ico,json}"],
+        rename: (dest, src) => {
+          return `${dest}${src}.gz`;
         },
-        expand: true,
-        cwd: "out/",
-        dest: "out/",
-        extDot: "last",
-        src: ["**/*.css"],
-        ext: ".css.gz",
       },
-      html_gz: {
-        options: {
-          mode: "gzip",
-          level: 9,
-        },
-        expand: true,
-        cwd: "out/",
-        dest: "out/",
-        extDot: "last",
-        src: ["**/*.html"],
-        ext: ".html.gz",
-      },
-      ico_gz: {
-        options: {
-          mode: "gzip",
-          level: 9,
-        },
-        expand: true,
-        cwd: "out/",
-        dest: "out/",
-        extDot: "last",
-        src: ["**/*.ico"],
-        ext: ".ico.gz",
-      },
-      js_br: {
+      create_br: {
         options: {
           mode: "brotli",
           brotli: {
@@ -61,63 +27,14 @@ module.exports = function (grunt) {
         cwd: "out/",
         dest: "out/",
         extDot: "last",
-        src: ["**/*.js"],
-        ext: ".js.br",
-      },
-      css_br: {
-        options: {
-          mode: "brotli",
-          brotli: {
-            quality: 11,
-          },
+        src: ["**/*.{js,css,html,ico,json}"],
+        rename: (dest, src) => {
+          return `${dest}${src}.br`;
         },
-        expand: true,
-        cwd: "out/",
-        dest: "out/",
-        extDot: "last",
-        src: ["**/*.css"],
-        ext: ".css.br",
-      },
-      html_br: {
-        options: {
-          mode: "brotli",
-          brotli: {
-            quality: 11,
-          },
-        },
-        expand: true,
-        cwd: "out/",
-        dest: "out/",
-        extDot: "last",
-        src: ["**/*.html"],
-        ext: ".html.br",
-      },
-      ico_br: {
-        options: {
-          mode: "brotli",
-          brotli: {
-            quality: 11,
-          },
-        },
-        expand: true,
-        cwd: "out/",
-        dest: "out/",
-        extDot: "last",
-        src: ["**/*.ico"],
-        ext: ".ico.br",
       },
     },
   });
 
   grunt.loadNpmTasks("grunt-contrib-compress");
-  grunt.registerTask("default", [
-    "compress:js_gz",
-    "compress:css_gz",
-    "compress:html_gz",
-    "compress:ico_gz",
-    "compress:js_br",
-    "compress:css_br",
-    "compress:html_br",
-    "compress:ico_br",
-  ]);
+  grunt.registerTask("default", ["compress:create_gz", "compress:create_br"]);
 };
